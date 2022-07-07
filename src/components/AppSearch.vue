9f0dec0fe024120859b0933990169fc47af7aea7
Joana Portugal <joanacportugal@gmail.com>
2022-07-07 14:47:08 +0100

    search as component

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="d-flex align-items-center"
+    style="width: 82vw; border-bottom: 1px solid #707070"
+  >
+    <b-input-group class="col-10">
+      <template #prepend>
+        <div
+          class="d-flex flex-row justify-content-center align-items-center mx-3"
+        >
+          <span class="material-icons-round" style="font-size: 32px"
+            >search</span
+          >
+        </div>
+      </template>
+      <b-form-input
+        placeholder="Pesquisa alguma coisa..."
+        style="
+          background-color: #fdfdf3;
+          border: none;
+          color: #2b4141;
+          height: 10vh;
+          font-size: 25px;
+        "
+      ></b-form-input>
+    </b-input-group>
+    <div class="col-2 d-flex flex-row justify-content-end pr-5">
+      <b-button
+        class="d-flex flex-row justify content-center align-items-center shadow-sm rounded-circle p-2"
+        style="background-color: #e87461; border: none"
+      >
+        <span class="material-icons-round" style="font-size: 30px"
+          >notifications</span
+        >
+      </b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppSearch",
+};
+</script>
--- src/views/HomeView2.vue
+++ src/views/HomeView2.vue
@@ -4,21 +4,7 @@
         <SideBar />
       </aside>
       <main class="d-flex flex-column justify-content-between">
-        <div class="d-flex align-items-center" style="width:82vw; border-bottom: 1px solid #707070 ;">
-          <!--<span class="material-icons-round">search</span>
-          <b-input class="py-4 border border-0 col-12"/>-->
-          <b-input-group class="col-10">
-            <template  #prepend >
-              <div class="d-flex flex-row justify-content-center align-items-center mx-3">
-              <span class="material-icons-round" style="font-size:32px;">search</span>
-              </div>
-            </template>
-            <b-form-input placeholder="Pesquisa alguma coisa..." style="background-color:#FDFDF3;border:none;color:#2B4141;height:10vh;font-size:25px;"></b-form-input>
-          </b-input-group>
-          <div class="col-2 d-flex flex-row justify-content-end pr-5">
-            <b-button class="d-flex flex-row justify content-center align-items-center shadow-sm rounded-circle p-2" style="background-color:#E87461;border:none;"><span class="material-icons-round" style="font-size:30px;">notifications</span></b-button>
-          </div>
-        </div>
+        <AppSearch />
 
         <AppFooter />
       </main>
@@ -28,11 +14,12 @@
 <script>
 import SideBar from "@/components/SideBar.vue"
 import AppFooter from "@/components/AppFooter.vue"
+import AppSearch from "@/components/AppSearch.vue"
 
 export default {
   name: 'HomeView',
   components: {
-    SideBar, AppFooter
+    SideBar, AppFooter, AppSearch
   }
 }
 </script>
